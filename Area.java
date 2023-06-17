eee11625f56d7704fe58251166e9e49c3cc52e44
Rekha Rao <rekartik@gmail.com>
2023-06-17 16:31:26 -0400

    this program calculates area of circles

---
+++ Area.java
@@ -0,0 +1,14 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ float radious= 6.6f;
+ final float Pie = 3.14f;
+ double area =Pie * radious * radious;
+ System.out.println("Area of circle is :" +area);
+ 
+ 
+	}
+
+}
