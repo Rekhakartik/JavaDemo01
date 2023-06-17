4eed35a4f6e4d4654884e3594a99f0e09ee036b4
Rekhakartik <135787470+Rekhakartik@users.noreply.github.com>
2023-06-17 17:31:59 -0400

    Java example1 file

---
+++ JavaExample1.java
@@ -0,0 +1,16 @@
+public class JavaExample1 {
+
+	public static void main(String[] args) {
+		
+		int a = 127;
+		System.out.println(a);
+		float b = 2.5f;
+System.out.println(a);
+		char ch= 'A';
+		System.out.println(ch);
+		
+		
+		
+	}
+
+}
