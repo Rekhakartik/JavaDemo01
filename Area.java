c2ffa5f456cd104c54a11695a7527294a2ea99d1
Rekhakartik <135787470+Rekhakartik@users.noreply.github.com>
2023-06-20 16:55:23 -0400

    Updated Area.java, added ArraysDemo.java

--- Area.java
+++ Area.java
@@ -5,6 +5,7 @@ public class Area {
 		// TODO Auto-generated method stub
  float radious= 6.6f;
  final float Pie = 3.14f;
+ //we are calculating area of circle
  double area =Pie * radious * radious;
  System.out.println("Area of circle is :" +area);
  
---
+++ ArraysDemo.java
@@ -0,0 +1,31 @@
+
+public class ArraysDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//An array is a data structure which holds a group of variables together
+		//its size should be defined
+		// When array is created we cannot change size of array
+		int month_days[]; // type name_arr[]
+		month_days = new int [6]; // name_arr = new type [size]
+		month_days[0] = 31;
+		month_days[1] = 28;
+		month_days[2] = 31;
+		month_days[3] = 30;
+		month_days[4] = 31;
+		month_days[5] = 30;
+		for (int i= 0;i<=5;i++)
+		System.out.println(month_days[0]);
+		System.out.println(month_days[1]);
+		System.out.println(month_days[2]);
+		System.out.println(month_days[3]);
+		System.out.println(month_days[4]);
+		System.out.println(month_days[5]);
+	
+	
+	
+	
+	
+	}
+
+}
